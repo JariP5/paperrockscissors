1540695a6dca6cd6f53b67709e8b17cbfa9e835e
Jari Polm <jpolm@rollins.edu>
2025-04-24 11:39:08 +0200

    feat: add ComputerPlayer class to randomize move

---
+++ src/main/java/nl/hartwigmedicalfoundation/rps/player/ComputerPlayer.java
@@ -0,0 +1,25 @@
+package nl.hartwigmedicalfoundation.rps.player;
+
+import nl.hartwigmedicalfoundation.rps.Move;
+
+import java.util.function.Supplier;
+
+public class ComputerPlayer implements Player {
+    private final String name;
+    private final Supplier<Move> moveSupplier;
+
+    public ComputerPlayer(Supplier<Move> moveSupplier) {
+        this.name = "Bot";
+        this.moveSupplier = moveSupplier;
+    }
+
+    @Override
+    public Move chooseMove() {
+        return moveSupplier.get();
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
