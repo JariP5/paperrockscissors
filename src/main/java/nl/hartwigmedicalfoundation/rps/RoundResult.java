b56e59239d418f6246314b3d97a1a3cdd0f09614
Jari Polm <jpolm@rollins.edu>
2025-04-24 10:33:51 +0200

    feat: add RoundResult class to model outcome of a game round

---
+++ src/main/java/nl/hartwigmedicalfoundation/rps/RoundResult.java
@@ -0,0 +1,25 @@
+package nl.hartwigmedicalfoundation.rps;
+
+public class RoundResult {
+    private final Move player1Move;
+    private final Move player2Move;
+    private final GameResult result;
+
+    public RoundResult(Move player1Move, Move player2Move, GameResult result) {
+        this.player1Move = player1Move;
+        this.player2Move = player2Move;
+        this.result = result;
+    }
+
+    public Move getPlayer1Move() {
+        return this.player1Move;
+    }
+
+    public Move getPlayer2Move() {
+        return this.player2Move;
+    }
+
+    public GameResult getGameResult() {
+        return this.result;
+    }
+}
